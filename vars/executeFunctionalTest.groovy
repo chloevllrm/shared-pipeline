09f01875d8d450d014de0ca39a4b48216f9675f3
chloevllrm <76785646+chloevllrm@users.noreply.github.com>
2026-07-06 02:43:16 -0700

    Refactor PowerShell script for functional test execution

--- vars/executeFunctionalTest.groovy
+++ vars/executeFunctionalTest.groovy
@@ -10,21 +10,68 @@ def call(Map options) {
             )
         ]) {
 
-            powershell '''
-            $tokenResponse = Invoke-RestMethod `
+            powershell """
+            \$tokenResponse = Invoke-RestMethod `
               -Method POST `
               -Uri "https://wm-sandbox-auth-1.watermelon.us/realms/watermelon/protocol/openid-connect/token" `
               -ContentType "application/x-www-form-urlencoded" `
               -Body @{
                 client_id="web_app"
-                username=$env:WM_USERNAME
-                password=$env:WM_PASSWORD
+                username=\$env:WM_USERNAME
+                password=\$env:WM_PASSWORD
                 grant_type="password"
               }
 
-            Write-Host "TOKEN RECEIVED"
-            Write-Host $tokenResponse.access_token.Substring(0,20)
-            '''
+            \$token = \$tokenResponse.access_token
+
+            Write-Host "Authentication successful"
+
+            \$body = @{
+                packId = ${options.packId}
+                runNow = \$true
+                environmentId = ${options.environmentId}
+                buildId = ${options.buildId}
+                iteration = 1
+                headlessExecution = \$false
+                webAutId = ${options.webAutId}
+            } | ConvertTo-Json
+
+            \$response = Invoke-RestMethod `
+              -Method PUT `
+              -Uri "https://wm-sandbox-1.watermelon.us/services/wmuitestcontroller/api/test-packs/execute_v2" `
+              -Headers @{
+                 Authorization = "Bearer \$token"
+              } `
+              -ContentType "application/json" `
+              -Body \$body
+
+            \$runId = \$response.id
+
+            Write-Host "RUN ID: \$runId"
+
+            do {
+
+                Start-Sleep -Seconds 10
+
+                \$result = Invoke-RestMethod `
+                  -Method GET `
+                  -Uri "https://wm-sandbox-1.watermelon.us/services/wmuitestcontroller/api/test-runs/\$runId" `
+                  -Headers @{
+                     Authorization = "Bearer \$token"
+                  }
+
+                Write-Host "STATE: \$((\$result).state)"
+                Write-Host "STATUS: \$((\$result).status)"
+
+            } while (\$result.state -ne "COMPLETED")
+
+            if (\$result.status -eq "FAIL") {
+                throw "Watermelon execution failed"
+            }
+
+            Write-Host "Watermelon execution completed successfully"
+
+            """
         }
     }
 }
